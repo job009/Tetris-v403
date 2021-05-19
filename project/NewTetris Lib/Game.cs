7cd7dcb1272401201ed62a058f2880d55cc67b3f
JonMichael Book <43754165+job009@users.noreply.github.com>
2021-05-19 02:01:43 -0500

    Add files via upload

--- project/NewTetris Lib/Game.cs
+++ project/NewTetris Lib/Game.cs
@@ -2,83 +2,120 @@
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace NewTetris_Lib {
-  /// <summary>
-  /// Oracle game class controling the entire game
-  /// </summary>
-  public class Game {
+namespace NewTetris_Lib
+{
     /// <summary>
-    /// Current level the player is on - currently unused
+    /// Oracle game class controling the entire game
     /// </summary>
-    private int level;
-
-    /// <summary>
-    /// Flag to see if player is currently playing the level
-    /// and therefore level code should be running - currently unused
-    /// </summary>
-    private bool isPlaying;
-
-    /// <summary>
-    /// Current player score - currently unused
-    /// </summary>
-    private int score;
-
-    /// <summary>
-    /// Random object used to randomly select next shape
-    /// to appear in level
-    /// </summary>
-    private Random random;
-
-    /// <summary>
-    /// Current shape dropping onto the playing field
-    /// </summary>
-    public static Shape curShape;
+    public class Game
+    {
+        /// <summary>
+        /// Current level the player is on - currently unused
+        /// </summary>
+        public static int level = 1;
 
 
-    /// <summary>
-    /// Next shape to be displayed in the side field
-    /// and played after the current piece is placed
-    /// </summary>
-    public static Shape nextShape;
-
-    /// <summary>
-    /// Link to widget displaying the playing field. 
-    /// Used to place pieces and shapes inside of it.
-    /// </summary>
-    public static Control field;
+        private static int totalLines = 0;
 
 
-    /// <summary>
-    /// Link to widget displaying the playing field. 
-    /// Used to place pieces and shapes inside of it.
-    /// </summary>
-    public static Control next;
+        /// <summary>
+        /// Flag to see if player is currently playing the level
+        /// and therefore level code should be running - currently unused
+        /// </summary>
+        private bool isPlaying;
 
-    /// <summary>
-    /// Holds the image for a piece that is used to 
-    /// compose a shape. This is used so the New Tetris Library
-    /// can retrieve the image for a shape.
-    /// </summary>
-    public static Image imgPiece;
+        /// <summary>
+        /// Current player score - currently unused
+        /// </summary>
+        public static int score = 0;
 
-    /// <summary>
-    /// Default constructor initializing random field and setting
-    /// curShape to null
-    /// </summary>
-    public Game() {
-      random = new Random();
-      curShape = null;
-      nextShape = null;
+        /// <summary>
+        /// Random object used to randomly select next shape
+        /// to appear in level
+        /// </summary>
+        private Random random;
+
+        /// <summary>
+        /// Current shape dropping onto the playing field
+        /// </summary>
+        public static Shape curShape;
+
+
+        /// <summary>
+        /// Next shape to be displayed in the side field
+        /// and played after the current piece is placed
+        /// </summary>
+        public static Shape nextShape;
+
+        /// <summary>
+        /// Link to widget displaying the playing field. 
+        /// Used to place pieces and shapes inside of it.
+        /// </summary>
+        public static Control field;
+
+
+        /// <summary>
+        /// Link to widget displaying the playing field. 
+        /// Used to place pieces and shapes inside of it.
+        /// </summary>
+        public static Control next;
+
+        /// <summary>
+        /// Holds the image for a piece that is used to 
+        /// compose a shape. This is used so the New Tetris Library
+        /// can retrieve the image for a shape.
+        /// </summary>
+        public static Image imgPiece;
+
+        /// <summary>
+        /// Default constructor initializing random field and setting
+        /// curShape to null
+        /// </summary>
+        public Game()
+        {
+            random = new Random();
+            curShape = null;
+            nextShape = null;
+        }
+
+        public static void increaseScore(int linesCleared)
+        {
+
+            totalLines += linesCleared;
+            if (linesCleared == 1)
+            {
+                score += 10 * level;
+
+            }
+            else if (linesCleared == 2)
+            {
+                score += 30 * level;
+            }
+            else if (linesCleared == 3)
+            {
+                score += 60 * level;
+            }
+            else if (linesCleared == 4)
+            {
+                score += 100 * level;
+            }
+
+            if (totalLines % 10 == 0 && totalLines != 0)
+            {
+                level++;
+            }
+        }
+
+        /// <summary>
+        /// Generates the next shape to be put into the playing field
+        /// </summary>
+        public void NextShape()
+        {
+            int shapeNum = random.Next(7);
+            ShapeType shapeType = (ShapeType)shapeNum;
+            curShape = ShapeFactory.MakeShape(shapeType, true);
+            Game.next.Controls.Clear();
+
+        }
     }
-
-    /// <summary>
-    /// Generates the next shape to be put into the playing field
-    /// </summary>
-    public void NextShape() {
-      int shapeNum = random.Next(7);
-      ShapeType shapeType = (ShapeType)shapeNum;
-      curShape = ShapeFactory.MakeShape(shapeType);
-      nextShape = ShapeFactory.MakeShape(shapeType);
-    }
-  }
 }
--- project/NewTetris Lib/PlayingField.cs
+++ project/NewTetris Lib/PlayingField.cs
@@ -1,68 +1,132 @@
 ﻿using System;
+using System.Diagnostics;
 
-namespace NewTetris_Lib {
-  /// <summary>
-  /// Encodes information about the playing
-  /// field of the game. Uses a grid of rows
-  /// and cols storing 1 for occupied and 0
-  /// for vacant
-  /// </summary>
-  public class PlayingField {
+namespace NewTetris_Lib
+{
     /// <summary>
-    /// Singleton pattern instance
+    /// Encodes information about the playing
+    /// field of the game. Uses a grid of rows
+    /// and cols storing 1 for occupied and 0
+    /// for vacant
     /// </summary>
-    private static PlayingField instance = null;
+    public class PlayingField
+    {
+        /// <summary>
+        /// Singleton pattern instance
+        /// </summary>
+        private static PlayingField instance = null;
 
-    /// <summary>
-    /// Grid holding 1 for occupied, 0 for vacant
-    /// </summary>
-    public int[,] field;
+        private int linesCleared = 0;
 
-    /// <summary>
-    /// Observer pattern event for when a row is 
-    /// cleared - currently unused
-    /// </summary>
-    public event Action OnRowClear;
+        /// <summary>
+        /// Grid holding 1 for occupied, 0 for vacant
+        /// </summary>
+        public int[,] field;
 
-    /// <summary>
-    /// Default constructor initializing the field
-    /// to 22 rows and 15 columns
-    /// </summary>
-    private PlayingField() {
-      field = new int[22, 15];
+        /// <summary>
+        /// Observer pattern event for when a row is 
+        /// cleared - currently unused
+        /// </summary>
+        public event Action OnRowClear;
+
+        /// <summary>
+        /// Default constructor initializing the field
+        /// to 22 rows and 15 columns
+        /// </summary>
+        private PlayingField()
+        {
+            field = new int[22, 15];
+        }
+
+        /// <summary>
+        /// Retrieves the Singleton pattern instance
+        /// </summary>
+        /// <returns>The Singleton instance</returns>
+        public static PlayingField GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new PlayingField();
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Checks if a location in the field is empty (i.e. vacant)
+        /// </summary>
+        /// <param name="r">Row</param>
+        /// <param name="c">Column</param>
+        /// <returns>True if empty, False otherwise</returns>
+        public bool IsEmpty(int r, int c)
+        {
+            if (r < 0 || r >= field.GetLength(0) || c < 0 || c >= field.GetLength(1))
+            {
+                return false;
+            }
+            return field[r, c] == 0;
+        }
+
+
+        public void DropAboveRows(int r)
+        {
+            for (int r2 = r - 1; r2 >= 1; r2--)
+            {
+
+                for (int c = 0; c <= 14; c++)
+                {
+
+                    if (IsEmpty(r2, c) == false)
+                    {
+                        GetInstance().field[r2, c] = 0;
+                        GetInstance().field[r2 + 1, c] = 1;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks each row to see if any of them are filled and
+        /// needs to be cleared, then clears those rows
+        /// </summary>
+        public void CheckClearAllRows()
+        {
+
+            linesCleared = 0;
+            bool fullRow = true;
+            for (int r = 0; r <= 21; r++)
+            {
+
+                fullRow = true;
+                for (int c = 0; c <= 14; c++)
+                {
+
+                    if (IsEmpty(r, c) == true)
+                    {
+                        fullRow = false;
+                    }
+                }
+                if (fullRow == true)
+                {
+                    linesCleared++;
+
+                    for (int c = 0; c <= 14; c++)
+                    {
+
+                        GetInstance().field[r, c] = 0;
+
+                    }
+                    DropAboveRows(r);
+                    Piece.RemoveRowImg(r);
+
+                }
+            }
+            if (linesCleared >= 1)
+            {
+                Debug.WriteLine(linesCleared);
+                Game.increaseScore(linesCleared);
+                linesCleared = 0;
+
+            }
+        }
     }
-
-    /// <summary>
-    /// Retrieves the Singleton pattern instance
-    /// </summary>
-    /// <returns>The Singleton instance</returns>
-    public static PlayingField GetInstance() {
-      if (instance == null) {
-        instance = new PlayingField();
-      }
-      return instance;
-    }
-
-    /// <summary>
-    /// Checks if a location in the field is empty (i.e. vacant)
-    /// </summary>
-    /// <param name="r">Row</param>
-    /// <param name="c">Column</param>
-    /// <returns>True if empty, False otherwise</returns>
-    public bool IsEmpty(int r, int c) {
-      if (r < 0 || r >= field.GetLength(0) || c < 0 || c >= field.GetLength(1)) {
-        return false;
-      }
-      return field[r, c] == 0;
-    }
-
-    /// <summary>
-    /// Checks each row to see if any of them are filled and
-    /// needs to be cleared, then clears those rows - currently
-    /// unused and not implemented
-    /// </summary>
-    public void CheckClearAllRows() {
-
-    }
-  }
 }
